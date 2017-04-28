8d14b61fe60e8fe58c950860990600b6a77ccce4
Alexandr Dubovikov <alexandr.dubovikov@gmail.com>
2017-04-28 15:43:58 +0200

    added redis

--- core/dns.js
+++ core/dns.js
@@ -14,13 +14,15 @@ dns_server.on('request', function (request, response) {
   var name = request.question[0].name;
   var dns_type = parseInt(request.question[0].type);
   if (!name || !name.endsWith(dns_root)) { response.send(); return; }
-   
+  
+  console.log("TYPE",dns_type);
+  
   if (dns_type != 1 && dns_type != 35) { response.send(); return; }
   /* Resolve Target Number */
   //var target = name.replace(dns_root,"").split(".").join("").split("").reverse().join("");
 
   var target;
-	
+
   /* type ==  1 - IN A*/
   if(dns_type == 1) 
   {
@@ -77,3 +79,4 @@ dns_server.serve(dns_port,dns_host);
 /* Export */
 
 module.exports = app => dns_server;
+
